9e7fa4c809dd60d6e5e032f10d1b836dfb35edb4
Calin Capitanu <calincapitanu@gmail.com>
2021-12-18 13:32:03 +0100

    approximating

--- main.py
+++ main.py
@@ -15,6 +15,14 @@ TIME = 3
 
 displayed_data = [['Init'],[],[]]
 
+root = tk.Tk()
+root.configure(background = "white")
+root.geometry("1920x1080")
+bg = PhotoImage(file = "christmas.png")
+
+canvas = Canvas(root, width=1920, height=1080)
+canvas.pack(expand=YES, fill=BOTH)
+canvas.create_image(0, 0, image=bg, anchor=NW, tag="image")
 
 api_key_file = open("api_key", 'r')
 api_key = api_key_file.readlines()[0].strip("\n")
@@ -25,7 +33,7 @@ def get_data(key, minutes):
         r = requests.get(URL, timeout = 1)
         return r.text
     except:
-        return ""
+        return "Failed to retrieve data"
 
 def query_parser(data):
     try:
@@ -35,46 +43,78 @@ def query_parser(data):
         response_data = json_string["ResponseData"]
         buses = response_data["Buses"]
         rtn = []
-        for bus in buses:
-            rtn.append([bus["LineNumber"], bus["Destination"], bus["DisplayTime"]])
+        for bus in buses[:3]:
+            dt = bus["DisplayTime"]
+            if not "min" in dt and dt != "Nu":
+                now = datetime.datetime.now()
+                hour = str(now.hour)
+                if dt[:2] == hour[:2]:
+                    dt = abs(int(dt[3:5]) - int(now.minute))
+                else:
+                    dt = 60 - int(now.minute) + int(dt[3:5])
+                dt = "{} min".format(dt)
+                
+            rtn.append([bus["LineNumber"], bus["Destination"], dt])
         return rtn
     except:
         return []
+
+
+def failed_case():
+    print("FAILED CASE")
+    busses = []
+    for i in range(3):
+        bus = []
+        bus.append(canvas.itemcget(canvas.find_withtag("line{}0".format(i)), 'text'))
+        bus.append(canvas.itemcget(canvas.find_withtag("line{}1".format(i)), 'text'))
+        bus.append(canvas.itemcget(canvas.find_withtag("line{}2".format(i)), 'text'))
+        busses.append(bus)
+
+    print(busses)
+    for i in range(3):
+        canvas.delete("line{}0".format(i))
+        canvas.delete("line{}1".format(i))
+        canvas.delete("line{}2".format(i))
+
+    if busses[0][2] == "Nu":
+        busses = busses[1:]
+        busses[0][2] = "{} min".format(int(busses[0][2][:len(busses[0][2]) - 4]) - 1)
+        busses[1][2] = "{} min".format(int(busses[1][2][:len(busses[1][2]) - 4]) - 1)
+
+    elif "1" in busses[0][2]:
+        busses[0][2] = "Nu"
+        busses[1][2] = "{} min".format(int(busses[1][2][:len(busses[1][2]) - 4]) - 1)
+        busses[2][2] = "{} min".format(int(busses[2][2][:len(busses[2][2]) - 4]) - 1)
+    else:
+        busses[0][2] = "{} min".format(int(busses[0][2][:len(busses[0][2]) - 4]) - 1)
+        busses[1][2] = "{} min".format(int(busses[1][2][:len(busses[1][2]) - 4]) - 1)
+        busses[2][2] = "{} min".format(int(busses[2][2][:len(busses[2][2]) - 4]) - 1)
+
+    print(busses)
+    
+
+    for i, bus in enumerate(busses):
+        canvas.create_text(1920/6, 1080/6 + i*1080/3, fill="green", font=("Arial", 60), text=bus[0], tag="line{}0".format(i))
+        canvas.create_text(1920/6 + 1920/3, 1080/6 + i*1080/3, fill="green", font=("Arial", 60), text=bus[1], tag="line{}1".format(i))
+        canvas.create_text(1920/6 + 2*1920/3, 1080/6 + i*1080/3, fill="green", font=("Arial", 60), text=bus[2], tag="line{}2".format(i))
 
 
 def query_do():
     try:
         global test, displayed_data
         returned_data = get_data(api_key, 60)
+        if returned_data == "Failed to retrieve data":
+            failed_case()
+            return "failed case"
         parsed_data = query_parser(returned_data)
         now = datetime.datetime.now()
-        time = now.hour < 23 and now.hour > 8
-        for bus in parsed_data:
-            if bus[2] == '{} min'.format(TIME) or "{}:{}".format(now.hour, now.minute+TIME) == bus[2]:
-                if not np.array_equal(parsed_data[0], displayed_data[0]) and time:
-                    os.system("mpg123 3min.mp3 > /dev/null 2>&1")
-            if bus[2] == 'Nu' or "{}:{}".format(now.hour, now.minute) == bus[2]:
-                if not np.array_equal(parsed_data[0], displayed_data[0]) and time:
-                    os.system("mpg123 nu.mp3 > /dev/null 2>&1")
         displayed_data = parsed_data[:3]
         return parsed_data[:3]
     except:
         return []
 
 
-def repeat():
-    while True:
-        query_do()
-        time.sleep(3)
 
-root = tk.Tk()
-root.configure(background = "white")
-root.geometry("1920x1080")
-bg = PhotoImage(file = "christmas.png")
-
-canvas = Canvas(root, width=1920, height=1080)
-canvas.pack(expand=YES, fill=BOTH)
-canvas.create_image(0, 0, image=bg, anchor=NW, tag="image")
 
 def init_ui():
     data = [
@@ -93,17 +133,17 @@ def init_ui():
 
 
 def update_ui():
+
+    print("UPDATE UI")
     data = query_do()
 
-    
-    now = datetime.datetime.now()
-    time = now.hour < 23 and now.hour > 8
-    if len(data) == 0 and time:
-        root.after(300000, update_ui)    
-        return
-    elif len(data) == 0 and not time:
+    if data == "failed case":
         root.after(60000, update_ui)
         return
+        
+
+    now = datetime.datetime.now()
+    time = now.hour < 23 and now.hour > 8
 
     for i in range(3):
         canvas.delete("line{}0".format(i))
@@ -115,10 +155,11 @@ def update_ui():
         canvas.create_text(1920/6, 1080/6 + i*1080/3, fill="green", font=("Arial", 60), text=bus[0], tag="line{}0".format(i))
         canvas.create_text(1920/6 + 1920/3, 1080/6 + i*1080/3, fill="green", font=("Arial", 60), text=bus[1], tag="line{}1".format(i))
         canvas.create_text(1920/6 + 2*1920/3, 1080/6 + i*1080/3, fill="green", font=("Arial", 60), text=bus[2], tag="line{}2".format(i))
-    if time:
+    if not time:
         root.after(300000, update_ui)
     else:
-        root.after(60000, update_ui)    
+        root.after(60000, update_ui)
+    return
 
 
 
